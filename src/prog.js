2ae6e2eb62376f441b3d5c8a4a3b7876345c08a3
Ethan Luis McDonough <elmcdonough@comcast.net>
2018-06-05 14:22:07 -0500

    Optimizations

--- src/prog.js
+++ src/prog.js
@@ -1,9 +1,10 @@
 var img = (function() {
     var img = createGraphics($WIDTH, $HEIGHT, JAVA2D);
-    var data = $DATA.split(/([ÿ-ǽ][a-z\d]+)/).filter(function(s) { return s.length > 0; });
+    var data = $DATA.split(/([ÿ-ǽ][a-z\d]+)/);
     var index = 0;
     img.loadPixels();
     data.forEach(function(seq) {
+        if (seq === "") { return; }
         var value = seq[0].charCodeAt(0) - 255, count = +("0x" + seq.substr(1));
         for (var i = 0; i < count; i++) {
             img.imageData.data[index++] = value;
